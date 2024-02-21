dfdec19c99af681e20d0f19576a9cf36cea3a4be
har4s <khalvatihares@gmail.com>
2024-02-22 01:29:06 +0330

    integrate layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,27 @@
-import "styles/styles.scss";
-import React from "react";
-import type { AppProps } from "next/app";
-import { ManagedUI, FontGlobalCSS } from "components/ui";
+import 'styles/styles.scss'
+import React from 'react'
+import type { AppProps } from 'next/app'
+import { ManagedUI, FontGlobalCSS } from 'components/ui'
+
+const Noop: React.FC<{ children?: React.ReactNode }> = ({ children }) => (
+  <>{children}</>
+)
 
 export default function App({ Component, pageProps }: AppProps) {
   React.useEffect(() => {
-    document.body.classList?.remove("loading");
-  }, []);
+    document.body.classList?.remove('loading')
+  }, [])
+
+  const Layout = (Component as any).Layout || Noop
+
   return (
     <>
       <FontGlobalCSS />
       <ManagedUI>
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </ManagedUI>
     </>
-  );
+  )
 }
